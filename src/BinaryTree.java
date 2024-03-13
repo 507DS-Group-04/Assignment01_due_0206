9a5053703b7c452254f74cdc08e9268981322ab7
Longfei Li <297328694@qq.com>
2024-03-13 11:27:07 -0700

    feat:Assignment 6

---
+++ src/BinaryTree.java
@@ -0,0 +1,60 @@
+class Node {
+    int key;
+    Node left, right;
+
+    public Node(int item)
+    {
+        key = item;
+        left = right = null;
+    }
+}
+
+class BinaryTree {
+    Node root;
+    BinaryTree() { root = null; }
+    void printInorder(Node node)
+    {
+        if (node == null)
+            return;
+        printInorder(node.left);
+        System.out.print(node.key + " ");
+        printInorder(node.right);
+    }
+    void printPreorder(Node node)
+    {
+        if (node == null)
+            return;
+        System.out.print(node.key + " ");
+        printPreorder(node.left);
+        printPreorder(node.right);
+    }
+    void printPostorder(Node node)
+    {
+        if (node == null)
+            return;
+        printPostorder(node.left);
+        printPostorder(node.right);
+        System.out.print(node.key + " ");
+    }
+
+    public static void main(String[] args)
+    {
+        BinaryTree StackMaster = new BinaryTree();
+        StackMaster.root = new Node(1);
+        StackMaster.root.left = new Node(2);
+        StackMaster.root.right = new Node(3);
+        StackMaster.root.left.left = new Node(4);
+        StackMaster.root.left.right = new Node(5);
+        System.out.println(
+                "Inorder traversal of binary StackMaster is ");
+        StackMaster.printInorder(StackMaster.root);
+        System.out.println();
+        System.out.println(
+                "Preorder traversal of binary StackMaster is ");
+        StackMaster.printPreorder(StackMaster.root);
+        System.out.println();
+        System.out.println(
+                "Postorder traversal of binary StackMaster is ");
+        StackMaster.printPostorder(StackMaster.root);
+    }
+}
